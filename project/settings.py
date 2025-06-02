0e080ad20839b1ed84fa326e6e77b69a2bf5fc27
molli <mollidors@gmail.com>
2025-06-03 00:03:43 +0300

    Первый коммит

--- project/settings.py
+++ project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-l_!wy-z=!nh3pgbijo5@2)@^=8!fn50h5d)vwt!s&^7g$k!s!='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['nastt.pythonanywhere.com']
 
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
