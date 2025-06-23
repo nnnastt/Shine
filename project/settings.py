45593db4d8695b24ff86b67c0d6d655b90895ff4
molli <mollidors@gmail.com>
2025-06-23 07:26:37 +0300

    ФИНАЛ

--- project/settings.py
+++ project/settings.py
@@ -127,4 +127,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-LOGIN_REDIRECT_URL = '/profile/'
+LOGIN_REDIRECT_URL = '/catalog/'
